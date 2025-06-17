6a1c30d32cf57253615489132e7e73aba75f2f42
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-06-17 21:08:33 +0400

    Add files via upload

---
+++ 02-variables-and-data-types/variables.py
@@ -0,0 +1,23 @@
+# A variable holds a value. Python figures out the type automatically.
+a = 1
+b = 2.0
+c = "Hello, World!"
+d =1
+# Variables can be reassigned to different types.
+a = "Now I'm a string"
+print(a)  # Output: Now I'm a string
+print(b)  # Output: 2.0 
+
+
+a = 23
+
+aaa = 435
+
+arman = 34
+
+sameer = 45
+
+_samerr = 34
+
+# @sameer = 56 # Invalid due to @ symbol
+# s@meer # Invalid due to @ symbol
